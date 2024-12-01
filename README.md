d9176541a7984694d39dbc773044f9524e5d5747
hasan <hasanghorbel@gmail.com>
2024-12-01 13:18:21 +0100

    added README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Advent of Code 2024
+
+feel free to take a look
+
+## Performance
+### Day 1
+
+``
+Day 1
+---------------
+part1 : 277.50µs
+part2 : 372.70µs
+---------------
+``
---
+++ day1/target/release/deps/day1.d
@@ -0,0 +1,7 @@
+C:\Users\hasan\Desktop\aoc-2024\day1\target\release\deps\day1.exe: src\main.rs src\part1\mod.rs src\part2\mod.rs
+
+C:\Users\hasan\Desktop\aoc-2024\day1\target\release\deps\day1.d: src\main.rs src\part1\mod.rs src\part2\mod.rs
+
+src\main.rs:
+src\part1\mod.rs:
+src\part2\mod.rs:
