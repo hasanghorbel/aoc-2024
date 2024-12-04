ac9d275e8c4f5b2304a125e252fa767e18555db9
hasan <hasanghorbel@gmail.com>
2024-12-04 08:10:02 +0100

    finished day4

---
+++ day4/target/debug/deps/day4-e83cabf85dda5c1e.d
@@ -0,0 +1,9 @@
+C:\Users\hasan\Desktop\Archive\aoc-2024\day4\target\debug\deps\libday4-e83cabf85dda5c1e.rmeta: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../input.txt src\part2\../input.txt
+
+C:\Users\hasan\Desktop\Archive\aoc-2024\day4\target\debug\deps\day4-e83cabf85dda5c1e.d: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../input.txt src\part2\../input.txt
+
+src\main.rs:
+src\part1\mod.rs:
+src\part2\mod.rs:
+src\part1\../input.txt:
+src\part2\../input.txt:
