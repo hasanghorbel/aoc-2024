5fdab60bc5c05d98ae020402e922847c5072d4bb
hasan <hasanghorbel@gmail.com>
2024-12-10 20:20:09 +0100

    updated folder names

---
+++ day11/target/debug/deps/day11-fb9f36b99e38cedf.d
@@ -0,0 +1,9 @@
+C:\Users\hasan\Desktop\Archive\aoc-2024\day11\target\debug\deps\libday11-fb9f36b99e38cedf.rmeta: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../example.txt src\part2\../example.txt
+
+C:\Users\hasan\Desktop\Archive\aoc-2024\day11\target\debug\deps\day11-fb9f36b99e38cedf.d: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../example.txt src\part2\../example.txt
+
+src\main.rs:
+src\part1\mod.rs:
+src\part2\mod.rs:
+src\part1\../example.txt:
+src\part2\../example.txt:
