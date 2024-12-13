d80ed2728127428b8c3721798e4ac62042962761
Hasan Ghorbel <109307799+hasanghorbel@users.noreply.github.com>
2024-12-13 17:27:04 +0100

    Delete day04/target directory

--- day04/target/debug/deps/day4-0167c0f94af672d1.d
+++
@@ -1,9 +0,0 @@
-C:\Users\hasan\Desktop\Archive\aoc-2024\day4\target\debug\deps\day4-0167c0f94af672d1.exe: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../input.txt src\part2\../input.txt
-
-C:\Users\hasan\Desktop\Archive\aoc-2024\day4\target\debug\deps\day4-0167c0f94af672d1.d: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../input.txt src\part2\../input.txt
-
-src\main.rs:
-src\part1\mod.rs:
-src\part2\mod.rs:
-src\part1\../input.txt:
-src\part2\../input.txt:
--- day04/target/debug/deps/day4-1e3488afb9b9c179.d
+++
@@ -1,9 +0,0 @@
-C:\Users\hasan\Desktop\Archive\aoc-2024\day04\target\debug\deps\libday4-1e3488afb9b9c179.rmeta: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../input.txt src\part2\../input.txt
-
-C:\Users\hasan\Desktop\Archive\aoc-2024\day04\target\debug\deps\day4-1e3488afb9b9c179.d: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../input.txt src\part2\../input.txt
-
-src\main.rs:
-src\part1\mod.rs:
-src\part2\mod.rs:
-src\part1\../input.txt:
-src\part2\../input.txt:
