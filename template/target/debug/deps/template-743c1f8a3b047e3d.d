f3c82943fa96a7fdc718733a56d1e93b76a60118
hasan <hasanghorbel@gmail.com>
2024-12-13 17:42:49 +0100

    updated template

--- template/target/debug/deps/template-743c1f8a3b047e3d.d
+++
@@ -1,9 +0,0 @@
-C:\Users\hasan\Desktop\Archive\aoc-2024\template\target\debug\deps\libtemplate-743c1f8a3b047e3d.rmeta: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../example.txt src\part2\../example.txt
-
-C:\Users\hasan\Desktop\Archive\aoc-2024\template\target\debug\deps\template-743c1f8a3b047e3d.d: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../example.txt src\part2\../example.txt
-
-src\main.rs:
-src\part1\mod.rs:
-src\part2\mod.rs:
-src\part1\../example.txt:
-src\part2\../example.txt:
--- template/target/debug/deps/template-b012cef1aa69974a.d
+++
@@ -1,9 +0,0 @@
-C:\Users\hasan\Desktop\Archive\aoc-2024\template\target\debug\deps\libtemplate-b012cef1aa69974a.rmeta: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../example.txt src\part2\../example.txt
-
-C:\Users\hasan\Desktop\Archive\aoc-2024\template\target\debug\deps\template-b012cef1aa69974a.d: src\main.rs src\part1\mod.rs src\part2\mod.rs src\part1\../example.txt src\part2\../example.txt
-
-src\main.rs:
-src\part1\mod.rs:
-src\part2\mod.rs:
-src\part1\../example.txt:
-src\part2\../example.txt:
